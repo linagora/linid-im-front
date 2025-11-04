23b160d24bf8f9750461317d198dadde916db104
Zorin95670 <moittie.vincent@gmail.com>
2025-11-04 13:51:28 +0100

    feature: setup module-federation

---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,13 @@
+<template>
+  <q-page class="column items-center justify-evenly">
+    <div>test1 début</div>
+    <HelloWorld/>
+    <div>test1 fin</div>
+  </q-page>
+</template>
+
+<script setup lang="ts">
+import {defineAsyncComponent} from 'vue';
+
+const HelloWorld = defineAsyncComponent(() => import('catalog-ui/HelloWorld'));
+</script>
