713389452d667613e028665e3a493e9df2a9118e
Zorin95670 <moittie.vincent@gmail.com>
2025-11-04 13:51:28 +0100

    feature: setup module-federation

---
+++ src/types/federation.d.ts
@@ -0,0 +1,5 @@
+declare module 'catalog-ui/*' {
+  import type {DefineComponent} from 'vue';
+  const component: DefineComponent<Record<string, unknown>, Record<string, unknown>, unknown>;
+  export default component;
+}
