07930502c87c2c1f5eea184c8d9139df3ac5b69a
jcabannes <jordycabannes@msn.com>
2025-11-17 11:20:22 +0100

    chore: remove unnecessary auto-generated code and files

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -1,35 +1,5 @@
 import type { RouteRecordRaw } from 'vue-router';
 
-const routes: RouteRecordRaw[] = [
-  {
-    path: '/',
-    /**
-     * Main layout route
-     * @returns the MainLayout page component
-     */
-    component: () => import('layouts/MainLayout.vue'),
-    children: [
-      {
-        path: '',
-        /**
-         * Home page route
-         * @returns the IndexPage component
-         */
-        component: () => import('pages/IndexPage.vue'),
-      },
-    ],
-  },
-
-  // Always leave this as last one,
-  // but you can also remove it
-  {
-    path: '/:catchAll(.*)*',
-    /**
-     * Component to display when no matching route is found
-     * @returns the ErrorNotFound page component
-     */
-    component: () => import('pages/ErrorNotFound.vue'),
-  },
-];
+const routes: RouteRecordRaw[] = [];
 
 export default routes;
