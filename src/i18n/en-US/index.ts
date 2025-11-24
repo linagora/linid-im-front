7c65c5c65666685538d01a0637a6af636b1181c9
jcabannes <jordycabannes@msn.com>
2025-11-24 17:43:47 +0100

    feat: add homepage with layout, routing and i18n

--- src/i18n/en-US/index.ts
+++ src/i18n/en-US/index.ts
@@ -1,4 +1,10 @@
 // This is just an example,
 // so you can safely delete all default props below
 
-export default {};
+export default {
+  homepage: {
+    title: {
+      label: 'LinID Identity Manager',
+    },
+  },
+};
