958e760627c53dae74113f1ff5a8b8566e4642df
Vincent Moittie <moittie.vincent@gmail.com>
2025-12-24 10:59:41 +0100

    feat: load and configure ui-design

--- quasar.config.ts
+++ quasar.config.ts
@@ -13,7 +13,7 @@ export default defineConfig((ctx) => {
     // app boot file (/src/boot)
     // --> boot files are part of "main.js"
     // https://v2.quasar.dev/quasar-cli-vite/boot-files
-    boot: ['i18n', 'axios', 'remotes', 'module-lifecycle'],
+    boot: ['i18n', 'axios', 'ui-design', 'remotes', 'module-lifecycle'],
 
     // https://v2.quasar.dev/quasar-cli-vite/quasar-config-file#css
     css: ['app.scss'],
