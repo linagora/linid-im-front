9d02a91fc427093b07a69344bb5a0d34be4e780e
Adrien <acere@linagora.com>
2026-02-16 08:53:11 +0100

    feat: add restricted domains list API

---
+++ docker/mock-backend/src/data/restrictedDomains.js
@@ -0,0 +1,44 @@
+export default {
+  content: [
+    {
+      id: '10000000-0000-0000-0000-000000000000',
+      name: 'example.com',
+    },
+    {
+      id: '10000000-0000-0000-0000-000000000001',
+      name: 'example.org',
+    },
+    {
+      id: '10000000-0000-0000-0000-000000000002',
+      name: 'example.net',
+    },
+    {
+      id: '10000000-0000-0000-0000-000000000003',
+      name: 'example.test',
+    },
+    {
+      id: '10000000-0000-0000-0000-000000000004',
+      name: 'gmail.com',
+    },
+    {
+      id: '10000000-0000-0000-0000-000000000005',
+      name: 'yahoo.com',
+    },
+  ],
+  pageable: {
+    pageNumber: 0,
+    pageSize: 6,
+    offset: 0,
+    paged: true,
+    unpaged: false,
+  },
+  totalElements: 6,
+  totalPages: 1,
+  last: true,
+  first: true,
+  numberOfElements: 6,
+  sort: { sorted: false, unsorted: true, empty: true },
+  size: 6,
+  number: 0,
+  empty: false,
+};
